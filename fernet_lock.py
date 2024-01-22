d4085a612440bb185271108416d11d68cf18d871
Jonbd3 <jonbd3@gmail.com>
2024-01-22 08:45:18 -0500

    encrytion for files

---
+++ fernet_lock.py
@@ -0,0 +1,15 @@
+import cryptography.fernet
+import os
+
+# This is to learn about how encryption works, not to create malware.
+
+encryption_folder = os.path.expanduser("~/Documents/fernet_files")
+
+def file_finder(directory):
+    file_names = []
+    folder = os.listdir(directory)
+    file_names.append(folder)
+    print(file_names)
+
+
+file_finder(encryption_folder)
