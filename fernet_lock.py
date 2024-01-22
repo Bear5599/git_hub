4bb4be8b7f504d2b3c28a110f18f50e938388c91
Jonbd3 <jonbd3@gmail.com>
2024-01-22 15:02:50 -0500

    self contained

--- fernet_lock.py
+++ fernet_lock.py
@@ -1,17 +1,40 @@
-import cryptography.fernet
 import os
+from cryptography.fernet import Fernet
 
-# This is to learn about how encryption works, not to create malware.
 
+# WARNING READ THIS FILEPATH CAREFULLY!!!!!!
 encryption_folder = os.path.expanduser("~/Documents/fernet_files")
+# WARNING READ THIS FILEPATH CAREFULLY!!!!!!
 
 def file_finder(directory):
     file_names = []
     for file in os.listdir(directory):
         full_path = os.path.join(directory, file)
         if os.path.isfile(full_path):
-            file_names.append(file)
+            file_names.append(full_path)
     print(file_names)
+    return file_names
 
+def encrypter(encryption):
+    with open("crypt_key.key", "rb") as the_key:
+        encryption_key = the_key.read()
 
-file_finder(encryption_folder)
+    for file in encryption:
+        with open(file, "rb") as read_file:
+            contents = read_file.read()
+        contents_encryped = Fernet(encryption_key).encrypt(contents)
+        with open(file, "wb") as write_file:
+            write_file.write(contents_encryped)
+
+def decrypter(decryption):
+    # same variables since theyre in different functions
+    with open("crypt_key.key", "rb") as the_key:
+        encryption_key = the_key.read()
+
+    for file in decryption:
+        with open(file, "rb") as read_file:
+            contents = read_file.read()
+        contents_decryped = Fernet(encryption_key).decrypt(contents)
+        with open(file, "wb") as write_file:
+            write_file.write(contents_decryped)
+
