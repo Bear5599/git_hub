181fdd9dc41b11241909b7659bdccbc327496bf7
Jonbd3 <jonbd3@gmail.com>
2024-03-17 17:09:14 -0400

    fixed

--- snowflake/snowflake.py
+++ snowflake/snowflake.py
@@ -11,23 +11,6 @@ def drawKochSF(x1, y1, x2, y2, t):
     n = ((y1-y2)/d, (x2-x1)/d)
     p2 = (c[0]+h*n[0], c[1]+h*n[1])
 
-
-def draw_triangle(x1, y1, x2, y2, x3, y3, t):
-    # go to start of triangle
-    t.up()
-    t.setpos(x1, y1)
-    t.down()
-    t.setpos(x2, y2)
-    t.setpos(x3, y3)
-    t.setpos(x1, y1)
-    t.up()
-
-def main():
-    print("Testing turtle graphics")
-    t = turtle.Turtle()
-    t.hideturtle()
-
-    draw_koch = drawKochSF(-100, 0, 0, -173.2, 100)
     if d > 10:
         # flake #1
         drawKochSF(x1, y1, p1[0], p1[1], t)
@@ -41,21 +24,34 @@ def main():
     else:
         # draw cone
         t.up()
-        1 t.setpos(p1[0], p1[1])
+        t.setpos(p1[0], p1[1])
         t.down()
         t.setpos(p2[0], p2[1])
         t.setpos(p3[0], p3[1])
         # draw sides
         t.up()
-        2 t.setpos(x1, y1)
+        t.setpos(x1, y1)
         t.down()
         t.setpos(p1[0], p1[1])
         t.up()
-        3 t.setpos(p3[0], p3[1])
+        t.setpos(p3[0], p3[1])
         t.down()
         t.setpos(x2, y2)
 
-    turtle.mainloop()
+def main():
+    print('Drawing the Koch Snowflake...')
+    t = turtle.Turtle()
+    t.hideturtle()
+    # draw
+    try:
+        drawKochSF(-100, 0, 100, 0, t)
+        drawKochSF(0, -173.2, -100, 0, t)
+        drawKochSF(100, 0, 0, -173.2, t)
+    except:
+        print("Exception, exiting.")
+        exit(0)
+    # wait for user to click on screen to exit
+    turtle.Screen().exitonclick()
 
 if __name__ == "__main__":
     main()
